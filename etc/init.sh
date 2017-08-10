1394a0c4b79957fe4435492d1f6070215c407150
MoonFruit <dkmoonfruit@gmail.com>
2017-08-10 23:36:54 +0800

    add aria2
    Former-commit-id: 9971356c937a7085155762757e29f3ea1eedbb1c

--- etc/init.sh
+++ etc/init.sh
@@ -4,14 +4,24 @@ ln -sf Documents/Environment/etc/bashrc .bash_profile
 ln -sf Documents/Environment/etc/colorsvnrc .colorsvnrc
 ln -sf Documents/Environment/etc/git/gitconfig .gitconfig
 ln -sf Documents/Environment/etc/git/gitignore .gitignore
+ln -sf Documents/Environment/etc/inputrc .inputrc
 ln -sf Documents/Environment/etc/subversion .subversion
 ln -sf Documents/Environment/etc/vim .vim
 ln -sf Documents/Environment/etc/zshrc .zshrc
+touch Documents/Environment/etc/aria2/aria2.session
 
-mkdir -p .ssh
-cd .ssh || exit 1
+mkdir -p ~/.ssh
+cd ~/.ssh || exit 1
 ln -sf ../Documents/Environment/etc/ssh/config
+
+mkdir -p ~/Library/LaunchAgents
+cd ~/Library/LaunchAgents || exit 1
+ln -sf /Users/moon/Documents/Environment/etc/aria2/aria2.plist com.github.moonfruit.aria2.plist
+ln -sf /Users/moon/Documents/Environment/etc/stunnel/stunnel.plist com.github.moonfruit.stunnel.plist
 
 cd /usr/local/etc || exit 1
 ln -sf ~/Documents/Environment/etc/proxychains.conf
 ln -sf ~/Documents/Environment/etc/shadowsocks-libev.json
+
+cd /usr/local/etc/stunnel || exit 1
+ln -sf ~/Documents/Environment/etc/stunnel/stunnel.conf
