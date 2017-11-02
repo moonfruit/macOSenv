981826a80c2d637cb27a2807cdab44e46e2025a0
MoonFruit <dkmoonfruit@gmail.com>
2017-11-03 00:14:38 +0800

    upgrade third party repo
    Former-commit-id: 93d27c81527c6fc39886df10aa4b34d9f4dff28a

--- etc/zsh/java.zsh
+++ etc/zsh/java.zsh
@@ -1,6 +1,8 @@
 export JAVA_HOME=`/usr/libexec/java_home`
 export JRE_HOME="$JAVA_HOME/jre"
 
+export MAVEN_OPTS="-Xss2m"
+
 export JENKINS_URL="http://10.1.2.32/jenkins"
 
 # alias ant="JAVA_HOME=`/usr/libexec/java_home -v1.7` ant"
