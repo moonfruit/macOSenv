4f8e700dd2136cf114629c219d7bd93adb37655c
MoonFruit <dkmoonfruit@gmail.com>
2018-03-28 14:22:39 +0800

    Upgrade dependency
    Former-commit-id: 9f1041176608e2e04e8b2549c2995a65234eeab7

--- preferences/alfred/Alfred.alfredpreferences/workflows/user.workflow.95F5824A-836B-40CB-939F-BB816817392B/search.py
+++ preferences/alfred/Alfred.alfredpreferences/workflows/user.workflow.95F5824A-836B-40CB-939F-BB816817392B/search.py
@@ -12,19 +12,21 @@ def search(query):
     return response.json()
 
 def main(wf):
-    query = wf.args[0]
     try:
-        for contact in search(query):
-            title = None
-            if 'm_nsRemark' in contact and len(contact['m_nsRemark']) > 0:
-                title = contact['m_nsRemark']
-            else:
-                title = contact['m_nsNickName']
-            wf.add_item(title=title, subtitle=contact['m_nsNickName'], arg=contact['m_nsUsrName'], valid=True)
-        wf.send_feedback()
+        query = wf.args[0]
+        contacts = search(query)
+        if len(contacts) == 0:
+            wf.add_item(title='Empty results', subtitle='Please try another keyword', valid=False)
+        else:
+            for contact in contacts:
+                avatar = contact.get('wt_avatarPath')
+                remark = contact.get('m_nsRemark')
+                nickname = contact.get('m_nsNickName')
+                username = contact.get('m_nsUsrName')
+                wf.add_item(title=remark or nickname, subtitle=nickname, icon=avatar, arg=username, valid=True)
     except:
         wf.add_item(title='Failed to connect to the WeChat.', subtitle='Please open your WeChat.app and retry.', valid=False)
-        wf.send_feedback()
+    wf.send_feedback()
 
 if __name__ == '__main__':
     wf = Workflow()
