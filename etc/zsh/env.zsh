ac62762ec2315409ca50dadff125a9a37f40d3f2
MoonFruit <dkmoonfruit@gmail.com>
2018-05-19 14:55:37 +0800

    bug fix
    Former-commit-id: b5066f123aee413d01e32b33f999d7d9209a338d

--- etc/zsh/env.zsh
+++ etc/zsh/env.zsh
@@ -12,7 +12,7 @@ export XML_CATALOG_FILES="$ENV/package/vim/XMLCatalog/catalog.xml"
 DEFAULT_USER="moon"
 
 # for proxy
-if [[ -z "$no_proxy" ]]; then
-	printf -v no_proxy '%s,' 10.1.2.{1..255}
-	export no_proxy="localhost,127.0.0.1,${no_proxy}192.168.1.1,192.168.7.1"
-fi
+# if [[ -z "$no_proxy" ]]; then
+# 	printf -v no_proxy '%s,' 10.1.2.{1..255}
+# 	export no_proxy="localhost,127.0.0.1,${no_proxy}192.168.1.1,192.168.7.1"
+# fi
