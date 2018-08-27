e2b9a818ee9d0eaa01c1fe6669e81ad70f90e224
MoonFruit <dkmoonfruit@gmail.com>
2018-08-27 15:09:53 +0800

    Update
    Former-commit-id: 555341bdde22e210ea748a669bd6fe84a9d6aea5

---
+++ bin/findgo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+while getopts '0' OPT; do
+	case $OPT in
+		0) FLAGS+=(-print0);;
+		?) echo "Unknown flags" >&2; exit 1;;
+	esac
+done
+shift "$((OPTIND - 1))"
+
+DIR="$1"
+if [ -z "$DIR" ]; then
+	DIR=.
+fi
+
+find "$DIR" -not \( \
+	\( \
+		-not -name "." -name ".*" -o \
+		-name 'vendor' \
+	\) -prune \
+	\) -name "*.go" "${FLAGS[@]}"
