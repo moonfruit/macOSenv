def0786d4e4eb036e3e60fc2f35b4c32ccc1d2e2
MoonFruit <dkmoonfruit@gmail.com>
2018-10-22 16:26:31 +0800

    bug fix

--- bin/brew-analyze.py
+++ bin/brew-analyze.py
@@ -5,16 +5,27 @@ import json
 
 def analyze(input):
     data = json.load(input)
+    print("""@startuml homebrew
+skinparam usecase {
+    BorderColor DarkSlateGray
+    BackgroundColor DarkSeaGreen
+    BackgroundColor<< dep >> DarkGoldenRod
+}""")
     for obj in data:
         name = obj["name"]
-        print("(%s)" % name)
         installed = obj.get("installed")
         if installed:
             installed = installed[0]
+        is_dep = installed.get("installed_as_dependency")
+        if is_dep:
+            print("(%s) << dep >>" % name)
+        else:
+            print("(%s)" % name)
         deps = installed.get("runtime_dependencies")
         if deps:
             for dep in deps:
                 print("(%s)->(%s)" % (name, dep["full_name"]))
+    print("@enduml")
 
 
 if __name__ == "__main__":
@@ -28,5 +39,6 @@ if __name__ == "__main__":
                 analyze(f)
     else:
         from subprocess import Popen, PIPE
-        with Popen(["brew", "info", "--json=v1", "--installed"], stdout=PIPE) as proc:
+        with Popen(["brew", "info", "--json=v1", "--installed"],
+                   stdout=PIPE) as proc:
             analyze(proc.stdout)
