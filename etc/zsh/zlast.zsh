9d461564b40335669a8ad60b4bfcbce70ff7240c
MoonFruit <dkmoonfruit@gmail.com>
2018-12-18 11:23:11 +0800

    bug fix

--- etc/zsh/zlast.zsh
+++ etc/zsh/zlast.zsh
@@ -8,10 +8,8 @@ if [[ -f "$APP_HOME/etc/bashrc" ]]; then
 	NOCD=1 NOLANG=1 NOLUA=1 . "$APP_HOME/etc/bashrc"
 fi
 
-# for PATH
-# USER_PYTHON_HOME="/Users/moon/Library/Python/2.7"
-# export PATH="$USER_PYTHON_HOME/bin:$PATH"
-
+# for PATH and MANPATH
+PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
@@ -20,6 +18,12 @@ PATH="/usr/local/opt/file-formula/bin:$PATH"
 PATH="/usr/local/opt/ruby/bin:$PATH"
 PATH="/usr/local/lib/ruby/gems/2.5.0/bin:$PATH"
 export PATH="$ENV/bin:$PATH"
+
+MANPATH="/usr/local/opt/grep/libexec/gnuman:$MANPATH"
+MANPATH="/usr/local/opt/gnu-tar/libexec/gnuman:$MANPATH"
+MANPATH="/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
+MANPATH="/usr/local/opt/findutils/libexec/gnuman:$MANPATH"
+export MANPATH
 
 # Auto hash XXOO_HOME
 eval "$(env | grep _HOME | sed 's/\(.*\)=.*/hash -d \1="$\1"/')"
