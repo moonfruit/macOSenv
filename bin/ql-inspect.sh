2e72b9477f824285ae6d412c566a7d107856534f
MoonFruit <dkmoonfruit@gmail.com>
2020-12-16 21:12:43 +0800

    bug fix

---
+++ bin/ql-inspect.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+QL=${1%/}
+if [[ $QL == *.qlgenerator ]]; then
+	QL=$QL/Contents/Info.plist
+fi
+if [[ $QL != *.plist ]]; then
+	echo "Unknown $QL" >&2
+	exit 1
+fi
+
+PATTERN=$(plutil -convert json -o - "$QL" | jq -r '.CFBundleDocumentTypes[0].LSItemContentTypes | join("|")')
+if [[ -n $PATTERN ]]; then
+	qlmanage -m plugins | sort | rg "$PATTERN"
+fi
