7abfc7a9eadab629ca8278e522e05b856a645226
MoonFruit <dkmoonfruit@gmail.com>
2021-01-12 11:51:52 +0800

    Add brew-livecheck.sh

---
+++ bin/brew-livecheck.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ -z "$PROXY_ENABLED" ]] && hash proxy >/dev/null 2>&1; then
+	exec proxy "$0" "$@"
+fi
+
+BLUE=$(tput setaf 4)
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr0)
+
+brew livecheck --json --newer-only --installed | \
+	jq -r '.[] | select(.version.outdated) |
+		"\(if has("formula") then .formula else .cask end)\t\(.version.current)\t\(.version.latest)"' | \
+	while IFS=$'\t' read -r -a ITEMS; do
+		echo "$BLUE${ITEMS[0]}$RESET : ${ITEMS[1]} ==> $GREEN${ITEMS[2]}$RESET"
+	done
