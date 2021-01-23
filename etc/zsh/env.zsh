9c6b6358f0c1438eba93115353d6e3917955dccb
MoonFruit <dkmoonfruit@gmail.com>
2021-01-23 13:43:02 +0800

    bug fix

--- etc/zsh/env.zsh
+++ etc/zsh/env.zsh
@@ -19,7 +19,7 @@ DEFAULT_USER=moon
 if [[ -z "$no_proxy" ]]; then
 	printf -v no_proxy ',%s' 10.1.{1..3}.{1..254}
 	printf -v no_proxy2 ',%s' 192.168.1.{1..254}
-	export no_proxy="localhost,127.0.0.1${no_proxy}${no_proxy2}"
+	export no_proxy="localhost,.gingkoo,127.0.0.1${no_proxy}${no_proxy2}"
 fi
 
 # for crypto
