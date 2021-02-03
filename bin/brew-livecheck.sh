a5b0d0f6d8fa6c2e6b8b8c9bf118423410c68baf
MoonFruit <dkmoonfruit@gmail.com>
2021-02-03 19:49:37 +0800

    bug fix

--- bin/brew-livecheck.sh
+++ bin/brew-livecheck.sh
@@ -4,10 +4,13 @@ if [[ -z "$PROXY_ENABLED" ]] && hash proxy >/dev/null 2>&1; then
 	exec proxy "$0" "$@"
 fi
 
+BOLD=$(tput bold)
 BLUE=$(tput setaf 4)
 GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
 RESET=$(tput sgr0)
 
+echo "$RED==>$RESET ${BOLD}Live Check$RESET"
 brew livecheck --json --installed | \
 	jq -r '.[] | select(.version.outdated) |
 		"\(if has("formula") then .formula else .cask end)\t\(.version.current)\t\(.version.latest)"' | \
