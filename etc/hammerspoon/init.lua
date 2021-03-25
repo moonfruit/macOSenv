68579be0ab445a7552edf596a0d17763c29b961d
MoonFruit <dkmoonfruit@gmail.com>
2021-03-25 20:24:13 +0800

    Upgrade dependencies

--- etc/hammerspoon/init.lua
+++ etc/hammerspoon/init.lua
@@ -11,8 +11,11 @@
 local hstracker <const> = "net.hearthsim.hstracker"
 local hearthstone <const> = "unity.Blizzard Entertainment.Hearthstone"
 hs.application.watcher.new(function(name, event, app)
-	if event == hs.application.watcher.launching and app:bundleID() == hearthstone then
-		hs.application.open(hstracker)
-		hs.timer.doAfter(1, function() app:activate() end)
+	if event == hs.application.watcher.launching then
+		-- print(app:bundleID())
+		if app:bundleID() == hearthstone then
+			hs.application.open(hstracker)
+			hs.timer.doAfter(1, function() app:activate() end)
+		end
 	end
 end):start()
