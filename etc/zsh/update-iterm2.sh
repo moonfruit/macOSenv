a991b5b55d41dcbd29ff65122cec88f22bf83ce5
MoonFruit <dkmoonfruit@gmail.com>
2021-06-08 11:27:53 +0800

    bug fix

---
+++ etc/zsh/update-iterm2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh
+
+NAME=$(basename "$0")
+TMP=/tmp/$NAME.$$
+
+mkdir -p "$TMP"
+cd "$TMP" || exit 1
+
+copy() {
+    DEST="$2/$(basename "$1")"
+    if ! diff "$DEST" "$1"; then
+        cp "$1" "$DEST"
+    fi
+}
+
+wget https://iterm2.com/shell_integration/zsh -O item.zsh
+copy item.zsh "$ENV/etc/zsh"
+
+UTILS=(imgcat imgls it2api it2attention it2check it2copy it2dl it2getvar it2git it2setcolor it2setkeylabel it2ul it2universion)
+for UTIL in "${UTILS[@]}"; do
+    wget "https://iterm2.com/utilities/$UTIL"
+	BANG=$(head -1 "$UTIL")
+	if [[ $BANG == *python* ]]; then
+		sed -i '1s|.*|#!/usr/bin/env python|' "$UTIL"
+		copy "$UTIL" "$ENV/package/iterm2"
+	else
+    	copy "$UTIL" "$ENV/bin"
+	fi
+done
+
+rm -fr "$TMP"
