e5827c97df3b6563c2b472cb47c0efdc13866d07
MoonFruit <dkmoonfruit@gmail.com>
2021-09-02 18:17:53 +0800

    bug fix

--- bin/ql-inspect.sh
+++ bin/ql-inspect.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
-QL=${1%/}
-if [[ $QL == *.qlgenerator ]]; then
-	QL=$QL/Contents/Info.plist
-fi
-if [[ $QL != *.plist ]]; then
-	echo "Unknown $QL" >&2
-	exit 1
-fi
+inspect() {
+	QL=${1%/}
+	if [[ $QL == *.qlgenerator ]]; then
+		QL=$QL/Contents/Info.plist
+	fi
+	if [[ $QL != *.plist ]]; then
+		echo "Unknown $QL" >&2
+		exit 1
+	fi
 
-PATTERN=$(plutil -convert json -o - "$QL" | jq -r '.CFBundleDocumentTypes[0].LSItemContentTypes | join("|")')
-if [[ -n $PATTERN ]]; then
-	qlmanage -m plugins | sort | rg "$PATTERN"
-fi
+	PATTERN=$(plutil -convert json -o - "$QL" | jq -r '.CFBundleDocumentTypes[0].LSItemContentTypes | join("|")')
+	if [[ -n $PATTERN ]]; then
+		qlmanage -m plugins | sort | rg "$PATTERN"
+	fi
+}
+
+while [[ $# -gt 0 ]]; do
+	inspect "$1"
+	shift
+done
