14c89ab1c4282d9d6070da58b042c3a51a018c9b
MoonFruit <dkmoonfruit@gmail.com>
2022-09-01 15:52:45 +0800

    Update iterm2

--- etc/zsh/update-iterm2.sh
+++ etc/zsh/update-iterm2.sh
@@ -8,24 +8,24 @@ mkdir -p "$TMP"
 cd "$TMP" || exit 1
 
 copy() {
-    DEST="$2/$(basename "$1")"
-    if ! diff "$DEST" "$1"; then
-        cp "$1" "$DEST"
-    fi
+	DEST="$2/$(basename "$1")"
+	if ! diff "$DEST" "$1"; then
+		cp "$1" "$DEST"
+	fi
 }
 
 wget https://iterm2.com/shell_integration/zsh -O item.zsh
 copy item.zsh "$ENV/etc/zsh"
 
-UTILS=(imgcat imgls it2api it2attention it2check it2copy it2dl it2getvar it2git it2setcolor it2setkeylabel it2ul it2universion)
-for UTIL in "${UTILS[@]}"; do
-    wget "https://iterm2.com/utilities/$UTIL"
-	BANG=$(head -1 "$UTIL")
+UTILITIES=(imgcat imgls it2api it2attention it2check it2copy it2dl it2getvar it2git it2setcolor it2setkeylabel it2tip it2ul it2universion it2profile)
+for UTILITY in "${UTILITIES[@]}"; do
+	wget "https://iterm2.com/utilities/$UTILITY"
+	BANG=$(head -1 "$UTILITY")
 	if [[ $BANG == *python* ]]; then
-		sed -i '1s|.*|#!/usr/bin/env python|' "$UTIL"
-		copy "$UTIL" "$ENV/package/iterm2"
+		sed -i '1s|.*|#!/usr/bin/env python|' "$UTILITY"
+		copy "$UTILITY" "$ENV/package/iterm2"
 	else
-    	copy "$UTIL" "$ENV/bin"
+		copy "$UTILITY" "$ENV/bin"
 	fi
 done
 
