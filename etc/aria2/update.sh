e70a14f505f36c81ad3dff521108a0bda87e21f3
MoonFruit <dkmoonfruit@gmail.com>
2023-11-23 11:21:22 +0800

    bug fix

--- etc/aria2/update.sh
+++ etc/aria2/update.sh
@@ -8,5 +8,5 @@ TRACKERS=$(curl https://trackerslist.com/best_aria2.txt) || exit 1
 
 sed -i 's|^\(bt-tracker=\).*$|\1'"$TRACKERS"'|' aria2.conf
 
-http http://127.0.0.1:6800/jsonrpc id=$RANDOM method=aria2.changeGlobalOption \
+http -I http://127.0.0.1:6800/jsonrpc id=$RANDOM method=aria2.changeGlobalOption \
     params:='["token:yy1234",{"bt-tracker":"'"$TRACKERS"'"}]'
