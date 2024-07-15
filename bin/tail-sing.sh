6253a59259a377d5c83d7feaceb02547d24ab122
MoonFruit <dkmoonfruit@gmail.com>
2024-07-15 09:45:53 +0800

    bug fix

--- bin/tail-sing.sh
+++ bin/tail-sing.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-tail -F /tmp/moonfruit.sing.std{out,err} | rg -v www.gstatic.com
+tail -F /tmp/moonfruit.sing.std{out,err} | rg -v ' dns: | inbound/direct\[dns-in]: connection closed: '
