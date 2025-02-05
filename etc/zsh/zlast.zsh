967bf333dad6f23d172cbd574a777af1136cb2ef
MoonFruit <dkmoonfruit@gmail.com>
2025-02-05 11:10:48 +0800

    bug fix

--- etc/zsh/zlast.zsh
+++ etc/zsh/zlast.zsh
@@ -38,6 +38,7 @@ local prepend-path() {
 prepend-path "$BREW_PREFIX/opt/tscurl/bin"
 prepend-path "$BREW_PREFIX/opt/ruby/bin"
 prepend-path "$BREW_PREFIX/opt/python@3.13/libexec/bin"
+prepend-path "$BREW_PREFIX/opt/p7zip-all/bin"
 prepend-path "$BREW_PREFIX/opt/lsof/bin"
 prepend-path "$BREW_PREFIX/opt/grep/libexec/gnubin"
 prepend-path "$BREW_PREFIX/opt/gnu-tar/libexec/gnubin"
