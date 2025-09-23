c2842c9acb1946232b40bffdf309f847ff996a16
MoonFruit <dkmoonfruit@gmail.com>
2025-09-23 16:36:15 +0800

    bug fix

--- etc/config/nvim/lua/config/options.lua
+++ etc/config/nvim/lua/config/options.lua
@@ -2,10 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- if not vim.env.PROXY_ENABLED then
---   vim.env.http_proxy = "http://127.0.0.1:7890"
---   vim.env.https_proxy = "http://127.0.0.1:7890"
--- end
+if not vim.env.PROXY_ENABLED then
+  vim.env.http_proxy = "http://127.0.0.1:7890"
+  vim.env.https_proxy = "http://127.0.0.1:7890"
+end
 
 local local_dir = vim.env.HOME .. "/.local"
 vim.g.node_host_prog = local_dir .. "/node_modules/.bin/neovim-node-host"
