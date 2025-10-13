326c58e5ca514c99b6c81fcacb0433bf5d9c4df0
MoonFruit <dkmoonfruit@gmail.com>
2025-10-13 17:00:25 +0800

    bug fix

--- etc/config/nvim/lua/config/autocmds.lua
+++ etc/config/nvim/lua/config/autocmds.lua
@@ -5,7 +5,19 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("yy_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("text_wrap"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+  end,
+})
 
 vim.filetype.add({
   extension = {
