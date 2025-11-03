d04a19f6f3f06e88358b8e48d400fff543460369
MoonFruit <dkmoonfruit@gmail.com>
2025-11-03 18:34:43 +0800

    bug fix

--- etc/aria2/update.sh
+++ etc/aria2/update.sh
@@ -21,11 +21,12 @@ DIR=$(main-script-directory)
 create-temp-directory TEMP_DIR
 cd "$TEMP_DIR" || exit 1
 
-sed 's|^\(bt-tracker=\).*$|\1'"$TRACKERS"'|' "$DIR/aria2.conf" >aria2.conf
+#sed 's|^\(bt-tracker=\).*$|\1'"$TRACKERS"'|' "$DIR/aria2.conf" >aria2.conf
+echo "bt-tracker=$TRACKERS" >trackers.conf
 
 update-daemon() {
     proxy none curlie http://127.0.0.1:6800/jsonrpc id=$RANDOM method=aria2.changeGlobalOption \
         params:='["token:yy1234",{"bt-tracker":"'"$TRACKERS"'"}]' >&2
 }
 
-copy-if-diff aria2.conf "$DIR" update-daemon >/dev/null
+copy-if-diff trackers.conf "$DIR" update-daemon >/dev/null
